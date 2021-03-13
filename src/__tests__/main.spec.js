c5f118f9077a7806387606fb27afcc68eb7c191f
Victor Xinghai Zhou Purple <victor.develop@outlook.com>
2021-03-14 00:38:31 +0800

    :sparkles: Finishe first draft

---
+++ src/__tests__/main.spec.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { stream } = require("../main")
+
+describe('stream', () => {
+    test('works as expected', () => {
+        const getPage = async ([a, b] = [0, 0]) => {
+            return [a + 1, b + 3]
+        }
+        stream({
+            getPage,
+            hasNextPage: async ([x, y]) => (x + y < 10),
+            extractDataListFromPage: async (x) => x
+        }).flatten().toArray((arr) => {
+            expect(arr).toEqual([ 1, 3, 2, 6, 3, 9 ])
+        })
+    })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,38 @@
 type IterateAllPagesInput<Page, Item> = {
-  getPage: ((previousPage?: Page) => Promise<Page>) | ( (previousPage?: Page) => Page),
-  hasNextPage: ((previousPage?: Page) => Promise<boolean>) | ((previousPage?: Page) => Promise<boolean>),
-  extractDataListFromPage: ((p:Page) => Item[]) | ((p:Page) => Promise<Item[]>)
+  getPage: ((previousPage?: Page) => Promise<Page>),
+  hasNextPage: ((previousPage?: Page) => Promise<boolean>),
+  extractDataListFromPage: ((p: Page) => Promise<Item[]>)
 }
 
-type IterateAllPagesGenerator<Page, Item> = (args: IterateAllPagesInput<Page, Item>) => (maxPages: number) => AsyncGenerator<unknown[], void, unknown>
+import * as highland from 'highland'
 
-export const generator: IterateAllPagesGenerator<unknown, unknown>  = ({
-  getPage,
-  hasNextPage,
-  extractDataListFromPage
-}) => {
-  return async function* iterateAllPages(maxPages: number) {
-      let page;
-      for (let round = 0; round < maxPages; round++) {
-
-        if (round === 0) {
-          page = await getPage()
-        } else {
-          page = await getPage(page)
-        }
-
-        const dataList = await extractDataListFromPage(page)
-        yield dataList
-
-        if (!(await hasNextPage(page))) {
-          break;
-        }
-      }
-      return;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const generator = <Page extends unknown, Item extends unknown> ({getPage, hasNextPage, extractDataListFromPage}: IterateAllPagesInput<Page, Item>) => {
+  let page: undefined | Promise<Page>;
+  return function (push, next) {
+    const handleResolvedPage = resolvedPage => {
+      // push the data list into stream
+      extractDataListFromPage(resolvedPage).then(dataList => push(null, dataList))
+      hasNextPage(resolvedPage).then(resolvedHasNextPage => resolvedHasNextPage
+        // signify that this function can be called again for next page
+        ? next()
+        // signify the end of stream, no more data
+        : push(null, highland.nil))
+      return resolvedPage
+    }
+    if (page === undefined) {
+      // get the first page and resolve
+      page = getPage().then(handleResolvedPage)
+    } else {
+      // get the next page then resolve, set page to next page
+      page = page.then(getPage).then(handleResolvedPage)
+    }
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const streamAllPages = (args: IterateAllPagesInput<unknown, unknown>) => {
+  return highland(
+    generator(args)
+  )
 }
